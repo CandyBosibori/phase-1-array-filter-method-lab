3e4ee44c47ba888727b14c034863c6191b29757c
Candy <candy.ongaro@student.moringaschool.com>
2023-12-31 12:54:13 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(drivers, query) {
+    const matchingDrivers = drivers.filter(driver =>
+      driver.toLowerCase() === query.toLowerCase()
+    );
+  
+    return matchingDrivers;
+  }
+  function fuzzyMatch(drivers, query) {
+
+    const matchingDrivers = drivers.filter(driver =>
+      driver.toLowerCase().startsWith(query.toLowerCase())
+    );
+  
+    return matchingDrivers;
+  }
+  function matchName(drivers, query) {
+    
+    const matchingDrivers = drivers.filter(driver =>
+      driver.name.toLowerCase() === query.toLowerCase()
+    );
+  
+    return matchingDrivers;
+  }
